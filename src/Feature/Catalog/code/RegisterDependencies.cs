ec58918adb12e320efc4e93403f4ba6b015d4dc8
whuu <whuu@gazeta.pl>
2018-09-11 18:30:26 +0200

    Register dependencies

---
+++ src/Feature/Catalog/code/RegisterDependencies.cs
@@ -0,0 +1,18 @@
+﻿using ARCommerce.Feature.Catalog.Controllers;
+using ARCommerce.Feature.Catalog.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace ARCommerce.Feature.Catalog
+{
+	public class RegisterDependencies : IServicesConfigurator
+	{
+
+		public void Configure(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient<IARCatalogRepository, ARCatalogRepository>();
+			serviceCollection.AddTransient<ARCatalogController>();
+		}
+
+	}
+}
